f901715f67f5b74f5f26d0a7d823975ef5c0234f
Jennifer Lane <jennifer.lane610@gmail.com>
2022-05-28 12:52:38 +0100

    added some dummy data in seeds file to reference

---
+++ server/seeds.js
@@ -0,0 +1,32 @@
+// dummy data to reference
+
+const birds = [
+    {
+        id: 1,
+        name: "Robin",
+        realimg: "https://www.rspb.org.uk/globalassets/images/birds-and-wildlife/bird-species-illustrations/robin_adult_1200x675.jpg?preset=largelandscape_mobile",
+        cartoonimg: "Emma's robin pic",
+        cartoonimgfeet: "magnified feet lol",
+        activity: "day",
+        food: "seeds, insects",
+        fly: true,
+        feettype: "perch",
+        beaklength: "short",
+        beakwidth: "small",
+        feetinfo: "Short claws for perching on branches"
+    },
+    {
+        id: 2,
+        name: "Owl",
+        realimg: "https://petapixel.com/assets/uploads/2021/10/babybarnowl.jpg",
+        cartoonimg: "Emma's owl pic",
+        cartoonimgfeet: "magnified feet lol",
+        activity: "night",
+        food: "mice, insects",
+        fly: true,
+        feettype: "grasping",
+        beaklength: "short",
+        beakwidth: "medium",
+        feetinfo: "Sharp claws for hunting mice"
+    }
+]
