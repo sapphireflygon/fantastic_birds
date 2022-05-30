4c6709fb3eddffc9d8146f90bfa9bd13e1eea8f5
Jennifer Lane <jennifer.lane610@gmail.com>
2022-05-30 13:57:57 +0100

    display route through app.js and activities container to check dndactivity still works

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
+import ActivitiesContainer from "./containers/ActivitiesContainer";
 
 
 function App() {
   return (
     <div className="App">
-
+      <ActivitiesContainer />
     </div>
   );
 }
--- client/src/containers/ActivitiesContainer.js
+++ client/src/containers/ActivitiesContainer.js
@@ -1,11 +1,12 @@
 // contains the ActivityNav.js and the selected Activity.js
 
 import React from 'react';
+import DndActivity from '../components/DndActivity';
 
 const ActivitiesContainer = () => {
     return (
         <>
-
+            <DndActivity/>
         </>
     )
 };
