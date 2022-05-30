0bf53e614ef3dae68dfb4fbf12acd1897138d27a
Jennifer Lane <jennifer.lane610@gmail.com>
2022-05-30 14:43:17 +0100

    fixed some simple errors after merging

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,15 @@ import React from 'react';
 import OddOneOutActivity from './components/OddOneOutActivity';
 import GameColoring from './components/GameColoring';
 import './components/OddOneOutActivity.css'
-import './components/OddOneOutIMG/Bird.css'
-import ActivitiesContainer from './containers/ActivitiesContainer';
+import './components/Bird.css'
 
 function App() {
 
   return (
     <>
-    <div className="App">
-      <ActivitiesContainer/>
-    </div>
-    <div>
-        <div>
-          <OddOneOutActivity />
-        </div>
-        <div>
-          <GameColoring />
-        </div>
-    </div>
+      <div className="App">
+        <ActivitiesContainer/>
+      </div>
     </>
   );
 }
--- client/src/containers/ActivitiesContainer.js
+++ client/src/containers/ActivitiesContainer.js
@@ -2,16 +2,27 @@
 import React from "react";
 import PhotoMatchActivity from "../components/PhotoMatchActivity";
 import DndActivity from '../components/DndActivity';
+import OddOneOutActivity from "../components/OddOneOutActivity";
+import GameColoring from "../components/GameColoring";
 
 const ActivitiesContainer = () => {
     return (
         <>
-          <div>
-            <PhotoMatchActivity/>
-          </div>
-          <div>
-            <DndActivity/>
-          </div>
+            <div>
+                <PhotoMatchActivity />
+            </div>
+        <hr/>
+            {/* <div>
+                <DndActivity />
+            </div>
+        <hr/>
+            <div>
+                <OddOneOutActivity />
+            </div>
+        <hr/>
+            <div>
+                <GameColoring />
+            </div> */}
         </>
     )
 }
